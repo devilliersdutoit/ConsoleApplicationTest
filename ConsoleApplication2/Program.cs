23002f1682c7549a648868615bf0bed7a49da02c
devilliersdutoit <jacob.dutoit@googlemail.com>
2015-05-18 23:00:00 +0200

    f1

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world! what are you doing.br2");
+            Console.WriteLine("Hello, world! what are you doing.f1");
             Console.ReadLine();
         }
     }
