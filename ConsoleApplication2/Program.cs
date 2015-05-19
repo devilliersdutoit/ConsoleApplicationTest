3ace0757761dcd93532cfbe8db933b69988a8b55
devilliersdutoit <jacob.dutoit@googlemail.com>
2015-05-19 09:55:55 +0200

    Feature 2 - test1

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world! what are you doing.feature 34");
+            Console.WriteLine("Feature 2 - test1");
             
             Console.ReadLine();
         }
